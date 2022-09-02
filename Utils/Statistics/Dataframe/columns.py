707be24c84207cd18f5d1cdf99daea6e82ae8cfb
christianSistemiPos <dima9610@gmail.com>
2022-09-02 13:11:01 +0200

    Aggiunto metodo per bilanciare il dataset rispetto ai valori di una colonna
    TODO: Nel metodo c'e` da capire come valutare la threshold di bilanciamento

--- Utils/Statistics/Dataframe/columns.py
+++ Utils/Statistics/Dataframe/columns.py
@@ -1,7 +1,9 @@
 import logging
 import numpy as np
+import pandas as pd
+from typing import Dict, List
 
-def get_stats_of_each_column(dataset):
+def get_stats_of_each_column(dataset: pd.DataFrame) -> Dict:
     """Retrieve statistics of each column of the dataset
 
     Args:
@@ -23,7 +25,7 @@ def get_stats_of_each_column(dataset):
     return _dict
 
 
-def get_percentage_of_NaN_in_columns(dataset):
+def get_percentage_of_NaN_in_columns(dataset: pd.DataFrame) -> pd.Series:
     """Evaluate the percentage of NaN elements in each column of the dataset
 
     Args:
@@ -35,7 +37,7 @@ def get_percentage_of_NaN_in_columns(dataset):
     percentage_of_NaN_for_column = dataset.isnull().sum() *100/dataset.shape[0]
     return percentage_of_NaN_for_column
 
-def filter_NaN_values_by_threshold(dataset, threshold = 20):
+def filter_NaN_values_by_threshold(dataset: pd.DataFrame, threshold: int = 20) -> pd.Series:
     """Retrieve only the columns of the dataset that have less than the given threshold of NaN elements
 
     Args:
@@ -46,15 +48,44 @@ def filter_NaN_values_by_threshold(dataset, threshold = 20):
         pd.Series: The columns that match the condition
     """    
     null_columns = get_percentage_of_NaN_in_columns(dataset)
-    filtered_cols = null_columns[null_columns < 20]
+    filtered_cols = null_columns[null_columns < threshold]
     return filtered_cols
 
-def distribution_of_values_in_column(dataset, target_column):
-    """_summary_
+def distribution_of_values_in_column(dataset: pd.DataFrame, target_column: str, percentage: bool = True) -> pd.Series:
+    """Evaluate the distribution of each different value for a given column
 
     Args:
-        dataset (_type_): _description_
-        target_column (_type_): _description_
+        dataset (pd.DataFrame): Source dataset
+        target_column (str): Column for which the distribution is evaluated
+        percentage (bool, optional): Whether the distribution is a percentage or absolute value
+        
+    Returns:
+        pd.Series: The distribution in percentage of each different value 
     """    
-    return (dataset.groupby(target_column)[target_column].count() / dataset.shape[0])*100 
+    counter = dataset.groupby(target_column)[target_column].count()
+    return (counter / dataset.shape[0])*100 if percentage else counter
 
+def balance_dataset_wrt_column_values(dataset: pd.DataFrame, target_column: str, bf_cutting: str = True) -> pd.DataFrame:
+    """Balance a given dataframe wrt different values of a column
+
+    Args:
+        dataset (pd.DataFrame): Source dataframe
+        target_column (str): The column for which the balance is evaluated
+        bf_cutting (str, optional): Try to balance with a percentage or absolute value. Defaults to True.
+
+    Returns:
+        pd.DataFrame: _description_
+    """    
+    value_distribution = distribution_of_values_in_column(dataset, target_column, percentage=False)
+    threshold = 1000 # TODO: shold be absolute or a percentage???
+    if bf_cutting: threshold = min(value_distribution)
+    balanced_dataset = dataset.groupby(target_column).apply(lambda group: group.sample(frac=1)[:threshold])
+    return balanced_dataset
+    
+
+if __name__ == '__main__':
+    import os
+    print(os.getcwd())
+    dataset = pd.read_csv("data/loan_data.csv")
+    balance_dataset_wrt_column_values(dataset, 'TARGET')
+    

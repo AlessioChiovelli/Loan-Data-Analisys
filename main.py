5f2ed2d7a459d5e9bda0fddf53c5c7a60ae515f2
christianSistemiPos <dima9610@gmail.com>
2022-09-04 13:30:25 +0200

    Renaming su question_1 e main del progetto
    no dsc.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 logging.basicConfig(filename="bigdara.log", level=logging.DEBUG)
 
 
-from Question_1 import main as question_1 
+from Question_1 import question_1 as question_1 
 if __name__ == '__main__':
     # columns_filtered = filter_NaN_values_by_threshold(dataset = dataset, threshold = 10)
     # stats = get_stats_of_each_column(dataset)

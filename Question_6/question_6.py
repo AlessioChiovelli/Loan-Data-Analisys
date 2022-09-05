e659b143ba1e6998e30e41c0d0b413b3d294396b
seancesare <sea.ces.laz@gmail.com>
2022-09-05 23:02:23 +0200

    added percentages for quest 6

--- Question_6/question_6.py
+++ Question_6/question_6.py
@@ -35,34 +35,56 @@ def evaluate():
     slices = list(temp)
     # explode = [0, 0, 0, 0, .15]  # if you wanna pull a slice from the center, uncomment this
 
-    # prepare plotting
-    colors = ["#004c6d","#3c6b88", # color palette found on https://www.learnui.design/tools/data-color-picker.html 
-            "#658ca4", "#8eaec1",
-            "#b8d2e0", "#e4f6ff" ]
+    # # prepare plotting
+    # colors = ["#004c6d","#3c6b88", # color palette found on https://www.learnui.design/tools/data-color-picker.html 
+    #         "#658ca4", "#8eaec1",
+    #         "#b8d2e0", "#e4f6ff" ]
 
-    plt.title("Number of People by Education Level")
-    plt.pie(slices, labels = edu_labels, # explode = explode, 
-        colors = colors[1:], wedgeprops = {"edgecolor": "black"})
-    plt.tight_layout(pad = .8)
-    plt.show()
+    # plt.title("Number of People by Education Level")
+    # plt.pie(slices, labels = edu_labels, # explode = explode, 
+    #     colors = colors[1:], wedgeprops = {"edgecolor": "black"})
+    # plt.tight_layout(pad = .8)
+    # plt.show()
 
 
-    # # Prepare plot
-    # figure, axes = plt.subplots(3, 1, sharex=True, figsize=(10, 5))
-    # figure.suptitle('Education distribution')
-    # axes[0].set_title('Gender: Male')
-    # axes[1].set_title('Gender: Female')
-    # axes[2].set_title('General View')
-    # #
+
+
+
+    # small_df = df[[target, edu]]
+    # print(small_df[df.TARGET == 1].count() * 100 / small_df[target].count())
+    # total = small_df.count()
+
+    # ord_edu_labels = ['Lower secondary', 'Secondary / secondary special', 
+    #     'Incomplete higher', 'Higher education', 'Academic degree']  # ordered by level
+
+    # ratios = {}
+    # # counting for each type of education levels the ratio between the positive TARGET and the total number of TARGET
+    # for edu_type in ord_edu_labels:
+    #     temp_df = small_df[small_df.NAME_EDUCATION_TYPE == edu_type]
+    #     ratios[edu_type] = round((100 * temp_df[temp_df.TARGET == 1].count()/ temp_df.count()), 2)
     
-    # # Make subplot for male distribution
-    # sns.histplot(df[edu].value_counts(), ax = axes[0], x = edu, kde = True, stat = "density", element = "step", bins = 100, binwidth = 1)
+    # ratios = pd.DataFrame(ratios)
+    # print(ratios)
 
+    # ratios.plot.bar()
+
+    # plt.style.use("fivethirtyeight")
+    # plt.title("Rate of Target Positivity by Education Level")
+    # plt.xlabel("Education")
+    # plt.ylabel("Rate of Insolvency (%)")
+
+    # plt.tight_layout()
+    # plt.show()
+
+    small_df_0 = df[[target, edu]].loc[df.TARGET == 0]
+    small_df_1 = df[[target, edu]].loc[df.TARGET == 1]
     
+    group0 = small_df_0.groupby(by=edu)
+    group1 = small_df_1.groupby(by=edu)
 
+
+    print(100 * group1.size()/group0.size())
+    print(group1.size())
     
-
-
-
-if __name__ == "__main__":
-    evaluate()
+    '''
+        sort_values(ascending=False)'''

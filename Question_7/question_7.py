058ffbb5f1a2aa4fe4fc7e6482bc485ed3f54578
christianSistemiPos <dima9610@gmail.com>
2022-09-06 14:08:01 +0200

    Aggiunto codice per Question 7
    Eliminato bigdara.log dal revisioning, viene mantenuto solo in locale
    Aggiunta voce in TODOs.txt
    Modificato .gitignore per non controllare immagini

---
+++ Question_7/question_7.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()  # Set style as seaborn even with 
+
+# @@ Personal Libraries
+from CONFIG import DATASET_PATH
+# @@
+
+def evaluate():
+    """Evaluate the question, and try to give an answer.
+    """    
+    dataset = pd.read_csv(DATASET_PATH)
+
+    # Prepare plot
+    figure, axes = plt.subplots(1, 2,  figsize=(15, 15))
+    figure.suptitle('Famility status and Insolvency')
+    axes[0].set_title('Family status distribution')
+    axes[1].set_title('Ratio of Insolvency for each Family Status')
+    
+    # Distribution of famility status
+    family_status_count = dataset["NAME_FAMILY_STATUS"].value_counts()
+    axes[0].pie(family_status_count.values.tolist(), labels= family_status_count.index.tolist(),autopct='%1.1f%%')
+    
+    # Evaluate ratio between the positive TARGET and the total number of TARGET for each family status
+    ratio =  dataset.groupby("NAME_FAMILY_STATUS").apply(lambda family_group: (100*family_group[family_group.TARGET == 1].count()/family_group["TARGET"].count())["TARGET"])
+    sns.barplot(x=ratio.index.tolist(), y=ratio.values.tolist(),ax=axes[1])
+    plt.show()
+    
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 
 from Question_1 import question_1 as question_1 
 from Question_6 import question_6 as question_6
+from Question_7 import question_7 as question_7
 if __name__ == '__main__':
     # columns_filtered = filter_NaN_values_by_threshold(dataset = dataset, threshold = 10)
     # stats = get_stats_of_each_column(dataset)
@@ -12,5 +13,8 @@ if __name__ == '__main__':
     # pretty_print_of_dict(stats, file_name="initial_stats")
 
     #question_1.evaluate()
-
+    question_1.evaluate()
     question_6.evaluate()
+    question_7.evaluate()
+
+

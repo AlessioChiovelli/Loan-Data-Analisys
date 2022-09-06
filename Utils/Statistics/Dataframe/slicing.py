cdc4bb9ae1ab542d923b6ade80fbb3aa64efb043
Alechiove <chiovelli.alessio@gmail.com>
2022-09-06 22:37:14 +0200

    (WIP) Folder Questions 2-5 + Mod Slicing
    Modificate le funzioni di slicing\nAggiunto le cartelle per rispondere ad alcune domande(con bozze di codice da rivedere)

--- Utils/Statistics/Dataframe/slicing.py
+++ Utils/Statistics/Dataframe/slicing.py
@@ -3,11 +3,11 @@ import seaborn as sns
 
 def unique_values_of_cols(dataset: pd.DataFrame, columns_to_select: list)-> dict:
     _dict = dict()
-    for col in columns:_dict[col] = dataset[col].unique()
+    for col in columns_to_select:_dict[col] = dataset[col].unique()
     return _dict
 
 
-def slicing_DF_target_based_on_columns_value(dataset : pd.DataFrame,columns: list, values_of_columns: dict, target_column : str) -> pd.DataFrame:
+def slicing_DF_target_based_on_columns_value(dataset : pd.DataFrame,columns: list, values_of_columns: dict, target_columns : list) -> pd.DataFrame:
     """
         Args:
             Columns: Columns we adopt to perform the slicing
@@ -15,23 +15,27 @@ def slicing_DF_target_based_on_columns_value(dataset : pd.DataFrame,columns: lis
         Returns:
             pd.DataFrame: A dataframe containing the values of the     
     """
-    df_to_slice = dataset[columns].copy()
-    print(df_to_slice)
+    all_columns = columns
+    all_columns.extend(target_columns)
+    df_to_slice = dataset[all_columns].copy()
+    # print(df_to_slice)
     
     for col in columns:
-        df_to_slice = df_to_slice.loc[df_to_slice[f'{col}'].isin(values_of_columns[col] if type(values_of_columns[col])==list else list(values_of_columns[col]))]
-    print(df_to_slice)
-    return df_to_slice
+        if col in list(values_of_columns.keys()):
+            df_to_slice = df_to_slice.loc[df_to_slice[f'{col}'].isin(values_of_columns[col] if type(values_of_columns[col])==list else list(values_of_columns[col]))]
+        else:continue
+    # print(df_to_slice[target_columns])
+    return df_to_slice[target_columns]
     
 if __name__ == '__main__':
-    dataset = pd.read_csv('/Users/alessio/Desktop/bigdata/data/loan_data.csv')
+    dataset = pd.read_csv('/Users/alessio/Desktop/Big Data/data/loan_data.csv')
     columns = ['CODE_GENDER','FLAG_OWN_CAR','CNT_CHILDREN','NAME_EDUCATION_TYPE']
     _dict = unique_values_of_cols(dataset,columns)
     print(_dict)
     _dict['CNT_CHILDREN'] = _dict['CNT_CHILDREN'][:3]
     _dict['FLAG_OWN_CAR'] = _dict['FLAG_OWN_CAR'][0]
     _dict['CODE_GENDER'] = _dict['CODE_GENDER'][0]
-    # print(dataset)
     print(_dict)
     
-    slicing_DF_target_based_on_columns_value(dataset = dataset, columns = columns, values_of_columns = _dict, target_column = 'TARGET')
+    df_sliced = slicing_DF_target_based_on_columns_value(dataset = dataset, columns = columns, values_of_columns = _dict, target_columns = ['TARGET'])
+    print(df_sliced)

cd81ffdc79cc92fa133a952c394b6cb9e87a6274
seancesare <sea.ces.laz@gmail.com>
2022-09-12 16:54:00 +0200

    added new question eval to main

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from Question_4 import question_4 as question_4
 from Question_6 import question_6 as question_6
 from Question_7 import question_7 as question_7
 from Question_8 import question_8 as question_8
+from Question_9 import question_9 as question_9
 from Question_10 import question_10 as question_10
+from Question_11 import question_11 as question_11
 from Question_12 import question_12 as question_12
 from Question_13 import question_13 as question_13
 from Question_14 import question_14 as question_14
@@ -29,9 +31,11 @@ if __name__ == '__main__':
     # question_4.evaluate()
     # question_6.evaluate()
     # question_7.evaluate()
-    question_8.evaluate()
+    # question_8.evaluate()
+    # question_9.evaluate()
     # question_10.evaluate()
+    question_11.evaluate()
     # question_12.evaluate()
     # question_13.evaluate()
     # question_14.evaluate()
-    question_15.evaluate()
+    # question_15.evaluate()

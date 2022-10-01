6db7f8a480960b75e02561f420854acbf6ad1885
seancesare <sea.ces.laz@gmail.com>
2022-10-01 20:18:00 +0200

    added insolvency rates barplot

--- Question_6/question_6.py
+++ Question_6/question_6.py
@@ -24,6 +24,12 @@ def evaluate():
         'Incomplete higher': 4, 
         'Lower secondary': 2, 
         'Academic degree': 6}  
+    
+    def _edu_level(level: str) -> int:
+        if level in edu_levels:
+            return edu_levels[level]
+        else:
+            return 0
 
     # for k,v in df[edu].value_counts().items():
     #     print(f"{k}: {v}")
@@ -47,44 +53,55 @@ def evaluate():
     plt.show()
 
 
+    # TARGET RATIO GROUPED BY EDUCATION LEVEL
+    df = df[[edu, target]]
+    rates = df.groupby(by=edu).mean() * 100
+    #print(rates)
+    rates["TARGET"].plot(x="Type of Education", y="Insolvency rate (%)", kind="bar")
+    plt.title("Insolvency rate grouped by Education Level")
+    plt.tight_layout(pad = .8)
+    plt.show()
 
 
 
-    # small_df = df[[target, edu]]
-    # print(small_df[df.TARGET == 1].count() * 100 / small_df[target].count())
-    # total = small_df.count()
+    # Beneath code that will be eliminated
 
-    # ord_edu_labels = ['Lower secondary', 'Secondary / secondary special', 
-    #     'Incomplete higher', 'Higher education', 'Academic degree']  # ordered by level
+    # # small_df = df[[target, edu]]
+    # # print(small_df[df.TARGET == 1].count() * 100 / small_df[target].count())
+    # # total = small_df.count()
 
-    # ratios = {}
-    # # counting for each type of education levels the ratio between the positive TARGET and the total number of TARGET
-    # for edu_type in ord_edu_labels:
-    #     temp_df = small_df[small_df.NAME_EDUCATION_TYPE == edu_type]
-    #     ratios[edu_type] = round((100 * temp_df[temp_df.TARGET == 1].count()/ temp_df.count()), 2)
+    # # ord_edu_labels = ['Lower secondary', 'Secondary / secondary special', 
+    # #     'Incomplete higher', 'Higher education', 'Academic degree']  # ordered by level
+
+    # # ratios = {}
+    # # # counting for each type of education levels the ratio between the positive TARGET and the total number of TARGET
+    # # for edu_type in ord_edu_labels:
+    # #     temp_df = small_df[small_df.NAME_EDUCATION_TYPE == edu_type]
+    # #     ratios[edu_type] = round((100 * temp_df[temp_df.TARGET == 1].count()/ temp_df.count()), 2)
     
-    # ratios = pd.DataFrame(ratios)
-    # print(ratios)
+    # # ratios = pd.DataFrame(ratios)
+    # # print(ratios)
 
-    # ratios.plot.bar()
+    # # ratios.plot.bar()
 
-    # plt.style.use("fivethirtyeight")
-    # plt.title("Rate of Target Positivity by Education Level")
-    # plt.xlabel("Education")
-    # plt.ylabel("Rate of Insolvency (%)")
+    # # plt.style.use("fivethirtyeight")
+    # # plt.title("Rate of Target Positivity by Education Level")
+    # # plt.xlabel("Education")
+    # # plt.ylabel("Rate of Insolvency (%)")
 
-    # plt.tight_layout()
-    # plt.show()
+    # # plt.tight_layout()
+    # # plt.show()
 
-    small_df_0 = df[[target, edu]].loc[df.TARGET == 0]
-    small_df_1 = df[[target, edu]].loc[df.TARGET == 1]
+    # small_df_0 = df[[target, edu]].loc[df.TARGET == 0]
+    # small_df_1 = df[[target, edu]].loc[df.TARGET == 1]
     
-    group0 = small_df_0.groupby(by=edu)
-    group1 = small_df_1.groupby(by=edu)
+    # group0 = small_df_0.groupby(by=edu)
+    # group1 = small_df_1.groupby(by=edu)
 
 
-    print(100 * group1.size()/group0.size())
-    print(group1.size())
+    # print(100 * group1.size()/group0.size())
+    # print(group1.size())
     
-    '''
-        sort_values(ascending=False)'''
+    # '''
+    #     sort_values(ascending=False)'''
+
